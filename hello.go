5641c7d2674bca7a7eff1c9f81b1672b0e107127
Thanh <thanh@Thanhs-MBP.fritz.box>
2019-01-29 12:13:25 +0100

    pointer empfänger

--- hello.go
+++ hello.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+
+	"github.com/user/stringutil"
+)
+
+// Pointer Empfänger
+
+type Vertex struct {
+	X, Y float64
+}
+
+func (v Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func (v *Vertex) Scale(f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
 
 func main() {
+	v := Vertex{3, 4}
+	v.Scale(10)
+	fmt.Println(v.Abs())
 	fmt.Println("Hello, world.")
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
+
 }
