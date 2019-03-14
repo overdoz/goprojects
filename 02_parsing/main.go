556e33358a6f66544c8145c008df401bc960ca7e
Thanh <thanh-le@hotmail.de>
2019-03-14 17:58:44 +0100

    set arguments when executing

--- 02_parsing/main.go
+++ 02_parsing/main.go
@@ -1,8 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"text/template"
+	"log"
+)
 
 func main() {
+	var message string
+	if len(os.Args) > 1 {
+		message = os.Args[1]
+	}
 
-	fmt.Println()
+	tpl, err := template.ParseFiles("test.gohtml")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	nf, err := os.Create("index.html")
+	if err != nil {
+		log.Println("error creating file", err)
+	}
+	defer nf.Close()
+
+	err = tpl.Execute(nf, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println(message)
+
+
 }
