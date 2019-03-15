1e37ff9af28845a803e212636954e6e8c5ef92c3
Thanh <thanh-le@hotmail.de>
2019-03-15 11:55:12 +0100

    my first server

---
+++ 04_server/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+)
+
+func main() {
+	li, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Panic(err)
+	}
+	defer li.Close()
+
+	for {
+		conn, err := li.Accept()
+		if err != nil {
+			log.Println(err)
+		}
+
+		io.WriteString(conn, "\nHello from Thanh\n")
+		fmt.Fprintln(conn, "My first server")
+		
+		conn.Close()
+	}
+}
