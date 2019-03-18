12358da575aeeb2af39dc1b1de6307fb6862e981
Thanh <thanh-le@hotmail.de>
2019-03-18 21:43:01 +0100

    write headers

---
+++ 13_write_headers/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type hotdog int
+
+func (m hotdog) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Thanh", "Author of this page")
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+
+
+	fmt.Fprintf(w, "<h1>any Code & %s </h1>", r.Method)
+}
+
+func main() {
+	var d hotdog
+	http.ListenAndServe(":8080", d)
+}
