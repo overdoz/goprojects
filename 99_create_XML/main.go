110b670f7df4639efa8ed9b690fab658699d2f7e
Thanh <thanh-le@hotmail.de>
2019-03-21 18:25:52 +0100

    create XML

--- 99_create_XML/main.go
+++ 99_create_XML/main.go
@@ -1,33 +1,69 @@
 package main
 
 import (
-	"encoding/xml"
-	"github.com/prometheus/common/log"
+	"fmt"
+	"github.com/beevik/etree"
+	"io/ioutil"
 	"os"
 )
 
-type VdpResponse struct {
-	XMLName xml.Name `xml:"vdpResponse"`
-	Header Header `cml:"header"`
-}
 
-type Header struct {
-	Action string `xml:"action"`
-}
-
-const xmlFile = "./EES.xml"
 
 func main() {
-	h := Header{"Test"}
-	v := VdpResponse{xml.Name{"ns1", "vdpResponse"}, h}
+	// el := etree.NewElement("ns1:")
+	doc := etree.NewDocument()
 
+	vdpRes := doc.CreateElement("ns1:vdpResponse")
+	header := vdpRes.CreateElement("ns1:header")
 
+	action := header.CreateElement("ns1:action")
+		action.CreateAttr("ns1:name", "DDA")
+		eeSnapshot := header.CreateElement("ns1:eeSnapshot")
+			eeSnapshot.CreateText("Test")
+		trackingID := header.CreateElement("ns1:trackingID")
+			trackingID.CreateText("Test")
+		sourceID := header.CreateElement("ns1:trackingID")
+			sourceID.CreateText("Test")
 
-	file,err := os.Create(xmlFile)
+	responseState := vdpRes.CreateElement("ns1:responseState")
+		responseState.CreateText("Test")
+
+	requestHeader := vdpRes.CreateElement("ns1:requestHeader")
+		action2 := requestHeader.CreateElement("ns1:action")
+		action2.CreateAttr("ns1:name", "DDA")
+		eeSnapshot2 := requestHeader.CreateElement("ns1:eeSnapshot")
+			eeSnapshot2.CreateText("Test")
+		trackingID2 := requestHeader.CreateElement("ns1:trackingID")
+			trackingID2.CreateText("Test")
+		sourceID2 := requestHeader.CreateElement("ns1:trackingID")
+			sourceID2.CreateText("Test")
+		market := requestHeader.CreateElement("ns1:market")
+			market.CreateText("Test")
+		tenant := requestHeader.CreateElement("ns1:tenant")
+			tenant.CreateText("Test")
+		user := requestHeader.CreateElement("ns1:user")
+			user.CreateText("Test")
+
+	language := requestHeader.CreateElement("ns1:language")
+		language.CreateText("Test")
+
+	payload := vdpRes.CreateElement("ns1:payload")
+
+	ees := etree.NewDocument()
+	xmlFile, err := os.Open("./test.xml")
 	if err != nil {
-		log.Fatalln(err)
+		fmt.Println(err)
+	}
+	byteValue, _ := ioutil.ReadAll(xmlFile)
+
+	if err := ees.ReadFromBytes(byteValue); err != nil {
+		panic(err)
 	}
 
-	xmlEnc := xml.NewEncoder(file)
-	xmlEnc.Encode(v)
+	payload.AddChild(ees)
+
+
+	doc.WriteToFile("./EES2.xml")
+
+
 }
