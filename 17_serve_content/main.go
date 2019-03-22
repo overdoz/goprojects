2f9ae78e1eb19e5b44788f84639974c6674feed2
Thanh <thanh-le@hotmail.de>
2019-03-22 13:28:11 +0100

    serve content

---
+++ 17_serve_content/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"os"
+)
+
+
+
+func dog(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "text/html; charset=utf-8")
+	io.WriteString(res, `<img src="/test.jpg">`)
+}
+
+
+
+func dogPic(res http.ResponseWriter, req *http.Request) {
+	f, err := os.Open("test.jpg")
+	if err != nil {
+		http.Error(res, "file not found", 404)
+		return
+	}
+	defer f.Close()
+
+	fi, err := f.Stat()
+	if err != nil {
+		http.Error(res, "file not found", 404)
+		return
+	}
+
+	http.ServeContent(res, req, fi.Name(), fi.ModTime(), f)
+}
+
+func main() {
+	http.HandleFunc("/", dog)
+	http.HandleFunc("/test.jpg", dogPic)
+	http.ListenAndServe(":8080", nil)
+}
