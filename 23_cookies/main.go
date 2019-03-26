3db4d469e8966e79755bde4e0ee4f04232175985
Thanh <thanh-le@hotmail.de>
2019-03-26 12:40:11 +0100

    read cookies

--- 23_cookies/main.go
+++ 23_cookies/main.go
@@ -26,11 +26,12 @@ func set(res http.ResponseWriter, req *http.Request) {
 }
 
 func read(res http.ResponseWriter, req *http.Request) {
-	http.SetCookie(res, &http.Cookie{
-		Name: "Hello Cookie",
-		Value: "Hello World",
-	})
-	fmt.Fprintln(res, "Not a virus")
-	fmt.Fprintln(res, "in chrome go to: dev tools / application / cookies")
+	c, err := req.Cookie("Hello Cookie")
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprintln(res, "Your cookie: ", c)
 
 }
